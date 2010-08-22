ab269b5c34644e74856230b257edded528fd096c
Koichiro Ohba <koichiro@meadowy.org>
2010-08-22 13:07:53 +0900

    Avoid const redefine warnings.

--- src/ruby/rjclass.rb
+++ src/ruby/rjclass.rb
@@ -16,17 +16,17 @@ require 'rjmethod'
 
 class RJClass
   
-  CONSTANT_Utf8    = 1
-  CONSTANT_Integer = 3
-  CONSTANT_Float   = 4
-  CONSTANT_Long    = 5
-  CONSTANT_Double  = 6
-  CONSTANT_Class   = 7
-  CONSTANT_String  = 8
-  CONSTANT_Fieldref= 9
-  CONSTANT_Methodref=10
-  CONSTANT_InterfaceMethodref = 11
-  CONSTANT_NameAndType        = 12
+  CONSTANT_Utf8    = 1 unless const_defined? :CONSTANT_Utf8
+  CONSTANT_Integer = 3 unless const_defined? :CONSTANT_Integer
+  CONSTANT_Float   = 4 unless const_defined? :CONSTANT_Float
+  CONSTANT_Long    = 5 unless const_defined? :CONSTANT_Long
+  CONSTANT_Double  = 6 unless const_defined? :CONSTANT_Double
+  CONSTANT_Class   = 7 unless const_defined? :CONSTANT_Class
+  CONSTANT_String  = 8 unless const_defined? :CONSTANT_String
+  CONSTANT_Fieldref= 9 unless const_defined? :CONSTANT_Fieldref
+  CONSTANT_Methodref=10 unless const_defined? :CONSTANT_Methodref
+  CONSTANT_InterfaceMethodref = 11 unless const_defined? :CONSTANT_InterfaceMethodref
+  CONSTANT_NameAndType        = 12 unless const_defined? :CONSTANT_NameAndType
 
   attr_reader :this_class
   attr_reader :super_class
